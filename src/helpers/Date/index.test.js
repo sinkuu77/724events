7aad3210fa91cb256e496976ed3eb2e18acb2f86
Minyoung GWON <sinkuu@naver.com>
2023-08-08 20:07:59 +0200

    Ajouté les tests unitaires

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,14 @@
-/**
- *
- */
+import { getMonth, MONTHS } from './index'
 
 describe("Date helper", () => {
   describe("When getMonth is called", () => {
     it("the function return janvier for 2022-01-01 as date", () => {
-      // to implement
+      const date = new Date("2022-01-01");
+      expect(getMonth(date)).toBe(MONTHS[1]);
     });
     it("the function return juillet for 2022-07-08 as date", () => {
-      // to implement
+      const date = new Date("2022-07-08");
+      expect(getMonth(date)).toBe(MONTHS[7]);
     });
   });
 });

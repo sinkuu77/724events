438ef1231513e19f6b1fab9eb42b2d4b71ea0a22
Minyoung GWON <sinkuu@naver.com>
2023-08-09 12:32:05 +0200

    Ajouté les tests d'intégration

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -54,11 +54,11 @@ const Page = () => {
           </div>
         </section>
         <section className="EventsContainer">
-          <h2 className="Title">Nos réalisations</h2>
+          <h2 className="Title" data-testid="events-test">Nos réalisations</h2>
           <EventList />
         </section>
         <section className="PeoplesContainer">
-          <h2 className="Title">Notre équipe</h2>
+          <h2 className="Title" data-testid="team-test">Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
           <div className="ListContainer">
             <PeopleCard
@@ -114,7 +114,7 @@ const Page = () => {
       </main>
       <footer className="row">
         <div className="col presta">
-          <h3>Notre derniére prestation</h3>
+          <h3 data-testid="last-event-title">Notre derniére prestation</h3>
           <EventCard
             imageSrc={last? last.cover : ""}
             title={last? last.title : ""}
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,6 +1,18 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
 
+const mockLastEvent = {
+  cover: "/images/headway-F2KRf_QfCqw-unsplash.png",
+  title: "Conférence #productCON",
+  date: new Date("2022-08-29T20:28:45.744Z"),
+};
+
+jest.mock("../../contexts/DataContext", () => ({
+  useData: () => ({
+    last: mockLastEvent,
+  }),
+}));
+
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
     render(<Home />);
@@ -27,16 +39,23 @@ describe("When Form is created", () => {
 });
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async() => {
+    render(<Home />);
+    await screen.findByTestId("events-test");
   });
-  it("a list a people is displayed", () => {
-    // to implement
+  it("a list a people is displayed", async() => {
+    render(<Home />);
+    await screen.findByTestId("team-test");
   });
-  it("a footer is displayed", () => {
-    // to implement
+  it("a footer is displayed", async() => {
+    render(<Home />);
+    await screen.findByText("Notre derniére prestation");
+    await screen.findByText("Contactez-nous");
   });
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+  it("an event card, with the last event, is displayed", async() => {
+    render(<Home />);
+    await screen.findByTestId("last-event-title");
+    await screen.findByText("Conférence #productCON");
+    await screen.findByText("août");
   });
 });
